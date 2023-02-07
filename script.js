d905d0658fb2f73dced7143f4bdc0c78123f59e0
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-07 21:42:10 +0300

    Преобразование к числу в JavaScript

--- script.js
+++ script.js
@@ -1,42 +1,14 @@
 "use strict";
-let a = '5' + '2';
-alert(a); // 52
+let a = '10';
+let b = '20';
+alert(Number(a) + Number(b));
 
-let b = '5' + 2;
-alert(b); //52
+alert( Number('2') + Number('3') ); // 5
 
-let c = 5 + '2';
-alert(c); // 52
+alert( 2 + Number('3') ); //5
 
-let d = 5 + 2;
-alert(d); // 7
+alert( '2' + Number('3') ); //23
 
-let e = '5' * '2';
-alert(e); //10
-
-let f = '5' - '2';
-alert(f); // 3
-
-let g = '5' / '2';
-alert(g); // 2.5
-
-let h = '5' % '2';
-alert(h); // 1
-
-let i = '5s' * '2';
-alert(i); // Nan
-
-let j = '5s' + '2';
-alert(j); // 5s2
-
-let k = (-'5') + (-'2');
-alert(k); //-7
-
-let l = '5' * 1 + '2' * 1;
-alert(l); // 7
-
-let m = '5' * '1' + '2' * '1';
-alert(m); // 7
-
-let n = '' + 3 + 1;
-alert(n); // 31
+let c = '2';
+let d = '3';
+alert(+c + +d);
