cc1511a6b413d5ec2e89af74ce3258399b49910b
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-07 17:13:29 +0300

    Математические операции с переменными в JS

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 "use strict";
-let a = 1 + 2 + 3;
-alert(a);
+let a = 10, b = 2;
+let sum = a + b;
+let min = a - b;
+let um = a * b;
+let del = a / b;
+alert(sum);
+alert(min);
+alert(um);
+alert(del);
+
+let c = 10, d = 5;
+let result = c + d;
+
+alert(result);
+
+let a1 = 1, b1 = 2, c1 = 3;
+alert(a1 + b1 + c1);
+
+let a2 = 10, b2 = 5;
+let c2 = a2 - b2;
+let d2 = 7;
+let result1 = c2 + d2;
+alert(result1);
 
 
