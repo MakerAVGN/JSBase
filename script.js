bfa7b21eba58198ecb5f06b5e824d3b3e8c16b29
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-07 20:59:17 +0300

    Сложение строк в JS

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 "use strict";
-let a = "Muhammed Ali";
-let b = "Kokiev";
-alert(a)
-alert(b)
+let a = "Hello world";
+alert(a + '!!!');
+
+let b = "java";
+let c = "script";
+alert(b + ' ' + c);
+
+let d = "hello";
+let e = "world";
+alert(d + ' ' + e);
