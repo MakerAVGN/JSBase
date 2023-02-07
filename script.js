3d439a2614704c2178092246feceb81655f16233
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-07 21:53:33 +0300

    Преобразование логического типа в JavaScript

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 "use strict";
-let a = parseFloat('5px');
-let b = parseFloat('6px');
-alert(a + b);
-
-let c = parseFloat('5.5px');
-let d = parseFloat('6.25px');
-alert(c + d);
-
-alert(c + d + 'px');
+alert(true + 3); // 4
+alert(true + true); // 2
+alert(true - true); // 0
+alert(true + false); // 1
+alert('1' + true); // 1true
+alert( String(true) + 1 );// true1
+alert( String(true) + Number(true) ); // true1
