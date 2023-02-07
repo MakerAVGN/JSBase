8e8174df79a6ff6512f47931837e5c2f4f6f58ab
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-07 22:31:34 +0300

    Последний символ строки в JavaScript

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 "use strict";
-let str = 'abcde';
-alert(str[0] + str[1] + str[2]); 
-
 let a = 'abcde';
-alert(a[4] + a[3] + a[2] + a[1] + a[0]);
+alert(a[a.length - 1]);
 
-let c= 'abcde';
-let num = 3;
-alert(c[num]);
+let b= 'abcde';
+alert(b[b.length - 2]);
+let c = 'abcde';
+alert(c[c.length - 3]);
