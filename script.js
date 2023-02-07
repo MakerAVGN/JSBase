dc86cab2bb81167148a6c49ff0b3c3774c6c8fcf
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-07 20:51:54 +0300

    Строки в JS

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 "use strict";
-let a = 2;
-alert(a ** 2);
-
-let b = 3 * 2 ** 3;
-alert(b); // 24
-
-let c = (3 * 2) ** 3;
-alert(c);  // 216
-
-let d = 3 * 2 ** (3 + 1);
-alert(d); // 48 
-
-let e = 2 ** 3 * 3;
-alert(e); // 24
-
-let f = 3 * 2 ** 3 * 3;
-alert(f); // 72
+let a = "Muhammed Ali";
+let b = "Kokiev";
+alert(a)
+alert(b)
