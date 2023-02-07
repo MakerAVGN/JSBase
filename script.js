a11e4d559406d8fb4ad08764fdf59184d583faab
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-07 21:18:44 +0300

    Специальные значения в JS

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 "use strict";
-let str = `a
-b
-c`;
+let str ;
 alert(str);
+
+let a = null;
+alert(a);
+
+let b = true;
+alert(b);
+
+let c = false;
+alert(c);
+
+let d = 'ab';
+let e = 'oba';
+alert('d' * 'e');
+
+let f = 10;
+alert(f / 0);
+
+let g = -10;
+alert(g / 0);
