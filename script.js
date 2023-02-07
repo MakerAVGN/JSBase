f7d43d954c8b3daed4e3f478539393c5b60f34d6
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-07 20:43:06 +0300

    Возведение в степень в JS

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 "use strict";
-let a = 13;
-let b = 5;
-alert(a % b);
+let a = 2;
+alert(a ** 2);
+
+let b = 3 * 2 ** 3;
+alert(b); // 24
+
+let c = (3 * 2) ** 3;
+alert(c);  // 216
+
+let d = 3 * 2 ** (3 + 1);
+alert(d); // 48 
+
+let e = 2 ** 3 * 3;
+alert(e); // 24
+
+let f = 3 * 2 ** 3 * 3;
+alert(f); // 72
