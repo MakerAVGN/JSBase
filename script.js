dd26d7dbb8470a599a67d108d517396894bb6f7f
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-07 20:31:50 +0300

    Дроби в JS

--- script.js
+++ script.js
@@ -1,36 +1,4 @@
 "use strict";
-let a = 5 + 5 * 3;
-alert(a); // 20
-
-let b = 5 + 5 * 3 + 3;
-alert(b); // 23
-
-let c = 8 / 2 + 2;
-alert(c); // 6
-
-let d = 8 + 2 / 2;
-alert(d); // 9
-
-let e = 8 / 2 * 2;
-alert(e); // 8
-
-let f = 8 * 4 / 2 / 2;
-alert(f); // 8
-
-let g = (2 + 3) * (2 + 3);
-alert(g); // 25
-
-let h = (2 + 3) * 2 + 3;
-alert(h); // 13
-
-let i = 2 * (2 + 4 * (3 + 1));
-alert(i); // 36
-
-let j = 2 * 8 / 4;
-alert(j); // 4
-
-let k = (2 * 8) / 4;
-alert(k); // 4
-
-let l = 2 * (8 / 4);
-alert(l);  // 4
+let a = 1.5;
+let b = 0.75;
+alert(a + b);
