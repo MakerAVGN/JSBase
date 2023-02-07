9079073ba780d8b72cae89d08f106410f10b670a
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-07 22:37:20 +0300

    Строки с цифрами в JavaScript

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 "use strict";
-let a = 'abcde';
-alert(a[a.length - 1]);
+let a = String(12345); 
+alert(a[0] + a[1] + a[2] + a[3] + a[4]);
 
-let b= 'abcde';
-alert(b[b.length - 2]);
-let c = 'abcde';
-alert(c[c.length - 3]);
+let b = String(12345); 
+alert(b[0] * b[1] * b[2] * b[3] * b[4]);
+
+let c = String(12345); 
+alert(c[4] + c[3] + c[2] + c[1] + c[0]);
+
+
