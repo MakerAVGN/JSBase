53f2d22a8678868f6b074de6ec47179eb06f2da9
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-07 21:33:04 +0300

    Константы в JS

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 "use strict";
-console.log(123);
-
-let num1 = 3, num2 = 4, num3 = 5;
-console.log(num1, num2, num3);
-
-let a = 'a', b = true, c = false, d = null, e = 1;
-console.log(a,b,c,d,e);
-
-alert(eeeeeeeeeeeeeeeeeeeeeeeee); // script.js?v=1:10 ошибка в 10 строке
+const PI = 3.14;
+let r = 5;
+let a = 2 * PI *  r;
+alert(a)
