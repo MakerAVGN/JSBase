f632c50634e6b34009994b6a48cb371221fb1968
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-23 00:52:14 +0300

    Добавление элементов по ключам JavaScript

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 "use strict";
 
-let arr = ['a', 'b', 'c'];
+let arr = [];
 
-arr[0] = '1';
-arr[1] = '-2';
-arr[2] = '-3';
-document.write(arr, '<br>');
+arr[0] = 'a';
+arr[1] = 'b';
+arr[2] = 'c';
 
 let arr1 = [1, 2, 3];
-arr1[0] += 3;
-arr1[1] += 3;
-arr1[2] += 3;
-document.write(arr1, '<br>');
+arr1[3] = '4';
+arr1[4] = '5';
 
-let arr2 = [1, 2, 3];
-arr2[0]++;
-arr2[1]++;
-arr2[2]++;
-document.write(arr2, '<br>');
+
+
