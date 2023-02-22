5483333069cb98eaff0cfd0cd8351d6a7c94ea2c
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-23 00:27:40 +0300

    Поиск ошибок в коде

--- script.js
+++ script.js
@@ -1,21 +1,52 @@
 "use strict";
-document.write('text');
-document.write('<br>');
+let num1 = 1;
+let num2 = 2;
+console.log('сумма: ' + (num1 + num2));
 
-document.write('<em>'+ 'text' +'</em    >');
-document.write('<br>');
+let a1 = 1;
+let b = 2;
+console.log(a1 + b);
 
-let str = 'text';
-document.write(str);
-document.write('<br>');
+let num3 = '123';
+let sum = Number(num3[0]) + Number(num3[1]) + Number(num3[2]);
+console.log(sum);
 
+let num4 = 123;
+num3 = String(num4);
+console.log(num3[0]);
 
-document.write('1');
-document.write('<br>');
-document.write('2');
-document.write('<br>');
-document.write('3');
-document.write('<br>');
-document.write('4');
-document.write('<br>');
-document.write('5');
+let a = 0;
+console.log(++a);
+
+let num5 = 123;
+num4 =  String(num5);
+console.log(num4.length);
+
+let a3 = 24 * 60 * 60;
+console.log(a3);
+
+let num6 = 123;
+let str = String(num6);
+console.log(str.length);
+
+let num7 = 123;
+let str2 = String(num7);
+console.log(Number(str2[str2.length - 1]));
+
+let num8 = 123;
+let str3 = String(num8);
+console.log(str3.length);
+
+let num9 = 123;
+let str4 = String(num9);
+console.log(str4[str4.length - 1]);
+
+let a4 = '123';
+let b2 = '456';
+let s = Number(a4) + Number(b2);
+console.log(s);
+
+let aaa = 1;
+let bbb = 2;
+let ccc = 3;
+console.log(aaa + bbb + ccc);
