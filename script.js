db21ebac961a978b1601733cbf96a6c5c92832f8
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-23 00:06:56 +0300

    Операции инкремента и декремента в JavaScript

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-// "use strict";
-// let num = 1;
-// num = num + 1;
-// num = num + 1;
+"use strict";
+// let num = 3;
+// alert(++num); //4
 
-// alert(num); // 3
+// let num1 = 3;
+// alert(num1++); // 3
 
-// let num1 = 1;
-// num1 = num1 + 2;
-// num1 = num1 + 3;
+// let num2 = 3;
+// alert(--num2); //2
 
-// alert(num1); // 6
+// let num1 = 3;
+// let num2 = ++num1;
+// alert(num1); // 4
+// alert(num2); // 4
 
+// let num1 = 3;
+// let num2 = num1++;
+// alert(num1); //4
+// alert(num2); //3
 
+// let num1 = 3;
+// let num2 = --num1;
+// alert(num1); //2
+// alert(num2); // 2
 
-let num = 47;
-num +=   7;
-num -= 18;
-num *=  10;
-num /= 15;
-alert(num);  //24
+// let num1 = 3;
+// let num2 = num1--;
+// alert(num1);  //2
+// alert(num2);  //3
+
+let num1 = 3;
+num1++;
+let num2 = num1--;
+alert(num1++); //3
+alert(--num2); //3   почему 3?
