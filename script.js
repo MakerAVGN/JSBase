3c909b5abf334268f1198a9fe07b6afc7b140a66
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-23 01:26:02 +0300

    Изменение элементов объектов в JavaScript

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
 "use strict";
 
-let obj = {
-	'1a': 1, // +
-	'b2': 2, // -
-	'с-с': 3, // +
-	'd 4': 4, // +
-	'e5': 5 // -
-};
+let obj = {x: 1, y: 2, z: 3};
+obj.x **= 2;
+obj.y **= 2;
+obj.z **= 2;
+console.log(obj);
 
-let obj = {
-	'1a': 1,
-	b2: 2,
-	'с-с': 3,
-	'd 4': 4,
-	e5: 5
-};
-
-console.log(obj['1a']);
-console.log(obj.b2);
-console.log(obj['c-c']);
-console.log(obj['d 4']);
-console.log(obj.e5);
