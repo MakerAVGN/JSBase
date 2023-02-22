fc6301fef4f0c361b3db0c7215ffd1becbf2526d
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-23 00:30:30 +0300

    Практика на операции в JavaScript

--- script.js
+++ script.js
@@ -1,52 +1,20 @@
 "use strict";
-let num1 = 1;
-let num2 = 2;
-console.log('сумма: ' + (num1 + num2));
+alert(60 * 60 * 24);
 
-let a1 = 1;
-let b = 2;
-console.log(a1 + b);
+alert(60 * 60 * 24 * 30);
 
-let num3 = '123';
-let sum = Number(num3[0]) + Number(num3[1]) + Number(num3[2]);
-console.log(sum);
+alert(60 * 60 * 24 * 365);
 
-let num4 = 123;
-num3 = String(num4);
-console.log(num3[0]);
+alert(60 * 24);
 
-let a = 0;
-console.log(++a);
+alert(60 * 365 * 24);
 
-let num5 = 123;
-num4 =  String(num5);
-console.log(num4.length);
+alert(1024 * 1024);
 
-let a3 = 24 * 60 * 60;
-console.log(a3);
+alert(1024 * 1024 * 1024);
 
-let num6 = 123;
-let str = String(num6);
-console.log(str.length);
+alert(1024 * 1024 * 1024 * 10);
 
-let num7 = 123;
-let str2 = String(num7);
-console.log(Number(str2[str2.length - 1]));
+alert(1024 * 1024 * 1000);
 
-let num8 = 123;
-let str3 = String(num8);
-console.log(str3.length);
-
-let num9 = 123;
-let str4 = String(num9);
-console.log(str4[str4.length - 1]);
-
-let a4 = '123';
-let b2 = '456';
-let s = Number(a4) + Number(b2);
-console.log(s);
-
-let aaa = 1;
-let bbb = 2;
-let ccc = 3;
-console.log(aaa + bbb + ccc);
+alert(1024 * 1024 * 1024 * 1000);
