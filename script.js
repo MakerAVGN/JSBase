eb98f5955e1c9e629235a4997bd9364533bb56c7
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-03-01 23:54:41 +0300

    Массивы как объекты в JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 "use strict";
 
+// console.log( typeof {x: 1, y: 2, z: 3} );4
+// object
 
-let obj = {x: 1, y: 2, z: 3};
-document.write( typeof obj['x'] );
+// let arr = [1, 2, 3];
+// console.log( typeof arr );
+// object
 
+// let arr = [1, 2, 3];
+// console.log( typeof arr[0] );
+// number
+
+let arr = ['1', '2', '3'];
+console.log( typeof arr[0] );
+//string
