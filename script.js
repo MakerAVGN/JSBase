977ca8bd039183273abb2499492785b8136f42c1
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-03-01 23:48:36 +0300

    Вычисляемые свойства в JavaScript

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
 "use strict";
-let a = 10, b = 2;
-let sum = a + b;
-let min = a - b;
-let um = a * b;
-let del = a / b;
-alert(sum);
-alert(min);
-alert(um);
-alert(del);
 
-let c = 10, d = 5;
-let result = c + d;
+let obj = { x: 1, y: 2, z: 3 };
+document.write(obj['x']);
 
-alert(result);
-
-let a1 = 1, b1 = 2, c1 = 3;
-alert(a1 + b1 + c1);
-
-let a2 = 10, b2 = 5;
-let c2 = a2 - b2;
-let d2 = 7;
-let result1 = c2 + d2;
-alert(result1);
+let obj1 = { x: 1, y: 2, z: 3 };
+let key = 'x';
+document.write(obj1[key]);
 
 
