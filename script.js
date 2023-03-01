5fc2c2b6338d2849b27ff8fc949204d010c47b15
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-03-02 00:08:49 +0300

    Неравенство по значению и типу в JavaScript

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 "use strict";
 
-let test1 = '123';
-let test2 = 123;
+//  let test1 = '3';
+// let test2 = '3';
+		
+// if (test1 == test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');   true
+// }
+// let test1 = '3';
+// let test2 = '3';
+		
+// if (test1 === test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');   true
+// }
+// let test1 = 3;
+// let test2 = '3';
+		
+// if (test1 == test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---'); // true
+// }
+// let test1 = 3;
+// let test2 = '3';
+		
+// if (test1 === test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');   false
+// }
 
-if (test1 == test2) {
+let test1 = 3;
+let test2 = 3;
+		
+if (test1 === test2) {
 	console.log('+++');
 } else {
-	console.log('---');
-}
+	console.log('---');   true
+} 
