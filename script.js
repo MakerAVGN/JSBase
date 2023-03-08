b1b5477c7ae2101e7871cce1b4c106ef927f2ae3
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-03-08 15:29:14 +0300

    Приоритет логических операторов в JavaScript

--- script.js
+++ script.js
@@ -1,68 +1,7 @@
-// let num1 = -10;
-// let num2 = -10;
+"use strict";
 
-// if (num1 >= 0 || num2 >= 0) {
-// 	console.log('+++');
-// } else {
-// 	console.log('---');   // +
-// }
-
-// let num1 = 0;
-// let num2 = 0;
-
-// if (num1 >= 0 || num2 >= 0) {
-// 	console.log('+++');  // +
-// } else {
-// 	console.log('---');
-// }
-
-// let num1 = 0;
-// let num2 = 5;
-
-// if (num1 >= 0 || num2 >= 0) {
-// 	console.log('+++');    // +
-// } else {
-// 	console.log('---');
-// }
-
-// let num1 = 5;
-// let num2 = 5;
-
-// if (num1 >= 0 || num2 >= 0) {
-// 	console.log('+++'); // +
-// } else {
-// 	console.log('---');
-// }
-
-// let num1 = -5;
-// let num2 = 15;
-
-// if (num1 >= 0 || num2 >= 0) {
-// 	console.log('+++');   // +
-// } else {
-// 	console.log('---');
-// }
-
-// let num = 1;
-
-// if (num == 0 || num == 1) {
-// 	console.log('+++');   // +
-// } else {
-// 	console.log('---');
-// }
-
-// let num = 2;
-
-// if (num == 0 || num == 1) {
-// 	console.log('+++');   
-// } else {
-// 	console.log('---');  // +
-// }
-
-let num = 2;
-
-if (num == 0 || num == 1 || num == 2) {
-	console.log('+++');   // +
+if (num == 0 || num > 1 && num < 5 ) {    // num == 0 ИЛИ  от 1 до 5
+	console.log('+++');
 } else {
 	console.log('---');
 }
