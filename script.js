bb9b8280e15b2680fab2ad2ecf186ed891e4a4b2
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-03-08 15:51:37 +0300

    Инвертирование логических выражений в JavaScript

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
 "use strict";
 
-// let num = 3;
-		
-// if (num > 5 && num < 10 || num == 20) {     // приоритет у num > 5 && num <10
-// 	console.log('+++');
-// } else {
-// 	console.log('---');
-// }
-
-// let num = 3;
-		
-// if (num > 5 || num > 0 && num < 3) {    // приоритет у num > 0 && num < 3
-// 	console.log('+++');
-// } else {
-// 	console.log('---');
-// }
-
-let num = 3;
-		
-if (num == 9 || num > 10 && num < 20 || num > 20 && num < 30) {   // приортитет у  num > 10 && num < 20 потом  num > 20 && num < 30
+if (!(num1 < 0 && num2 > 10)) {
 	console.log('+++');
 } else {
 	console.log('---');
