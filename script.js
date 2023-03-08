8587bdafff0a5fc4fa5f3d76b598f0cd206a2454
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-03-08 15:49:41 +0300

    Группировка условий в JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 "use strict";
 
-if (num == 0 || num > 1 && num < 5 ) {    // num == 0 ИЛИ  от 1 до 5
+// let num = 3;
+		
+// if (num > 5 && num < 10 || num == 20) {     // приоритет у num > 5 && num <10
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// let num = 3;
+		
+// if (num > 5 || num > 0 && num < 3) {    // приоритет у num > 0 && num < 3
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+let num = 3;
+		
+if (num == 9 || num > 10 && num < 20 || num > 20 && num < 30) {   // приортитет у  num > 10 && num < 20 потом  num > 20 && num < 30
 	console.log('+++');
 } else {
 	console.log('---');
