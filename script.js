841b42958cabbb1202a8d7c41d78b49ac279b6e9
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-03-08 16:07:52 +0300

    Сокращенная форма проверки на истину в JavaScript

--- script.js
+++ script.js
@@ -1,36 +1,9 @@
 "use strict";
 
-// let test = 1;
-		
-// if (test == true) {
-// 	console.log('+++');  // +
-// } else {
-// 	console.log('---');
-// }
+let test = true;
 
-
-// let test = 1;
-		
-// if (test == false) {
-// 	console.log('+++');
-// } else {
-// 	console.log('---');  // + 
-// }
-
-
-// let test;
-		
-// if (test == true) {
-// 	console.log('+++');  // +
-// } else {
-// 	console.log('---');
-// }
-
-
-let test = 3 * 'a';
-		
-if (test == true) {
+if (test) {
 	console.log('+++');
 } else {
-	console.log('---');   // +
+	console.log('---');
 }
